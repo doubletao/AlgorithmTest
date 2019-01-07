ac61d1abe95ce83bbb37372aff6ccff0f337625e
doubletao <doubletao@yeah.net>
2019-01-07 18:38:41 +0800

    题目。

--- subsetsum.h
+++ subsetsum.h
@@ -1,6 +1,10 @@
 #ifndef SUBSETSUM_H
 #define SUBSETSUM_H
 #include <vector>
+//给定一个随机的数字序列，
+//要求循环一次找出其中和最大的连续子集的和，
+//比如{6,-2,-3,5,9,-8,6}，
+//和最大的连续子集应该是6,-2,-3,5,9，和为15
 
 std::vector<int> GenerateVec();
 int FindSubSetSum(std::vector<int> & vecSrc);
